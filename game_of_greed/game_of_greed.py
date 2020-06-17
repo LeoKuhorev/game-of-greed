70d614ca65876c31d9d8b0cd04bd0a9213facd53
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-17 13:56:40 -0700

    classes created, test suites started

---
+++ game_of_greed/game_of_greed.py
@@ -0,0 +1,67 @@
+class GameLogic:
+    """Game Logic class"""
+
+    @staticmethod
+    def calculate_score(dice_roll: tuple) -> int:
+        """Calculating score of a given dice roll
+
+        Args:
+            dice_roll (tuple): Tuple of integers that represent a dice roll
+
+        Returns:
+            int: Roll’s score according to rules of game
+
+        Raises:
+            Exception: Not a tuple passed in
+            Exception: Tuple value is not integer
+        """
+        if type(dice_roll) is not tuple:
+            raise TypeError('Dice roll must be a tuple')
+        for roll in dice_roll:
+            if type(roll) is not tuple:
+                raise TypeError('Dice roll value must be integers')
+
+    @staticmethod
+    def roll_dice(number_of_dice: int) -> tuple:
+        """Generates a tuple of random values in range 1 - 6
+
+        Args:
+            number_of_dice (int): Number of dice to be rolled
+
+        Raises:
+            Exception: Value passed in is not an integer
+            Exception: Number of dice is out of range
+
+        Returns:
+            tuple: Random values in range 1 - 6 for each dice passed in
+        """
+        if type(number_of_dice) is not int:
+            raise TypeError('The number of dice must be an integer')
+
+        if (number_of_dice <= 0 or number_of_dice > 6):
+            raise ValueError('The number of dice must be between 1 and 6')
+
+
+class Banker:
+    """Class Banker"""
+
+    def shelf(self, points: int):
+        """Temporarily stores unbanked points
+
+        Args:
+            points (int): Number of points to store
+        """
+        pass
+
+    def bank(self) -> int:
+        """Permanently stores points from the shelf. Resets shelf points to 0
+
+        Returns:
+            int: The amount of points added to total from shelf
+        """
+        pass
+
+    def clear_shelf(self):
+        """Removes all unbanked points
+        """
+        pass
--- tests/test_game_of_greed.py
+++ tests/test_game_of_greed.py
@@ -1,5 +1,56 @@
+import pytest
+
 from game_of_greed import __version__
+from game_of_greed.game_of_greed import Banker, GameLogic
 
 
-def test_version():
+def test_proof_of_life():
     assert __version__ == '0.1.0'
+
+
+class TestGameLogic:
+    """Tests for the Game Logic class and its methods
+    """
+    @pytest.fixture()
+    def gl(self):
+        return GameLogic()
+
+    def test_gl_instance(self):
+        """Check class instantiation"""
+        assert GameLogic()
+
+    def test_gl_calculate_score_type_error_1(self):
+        """Test if the method raises TypeError when not a tuple passed in"""
+        with pytest.raises(TypeError) as err:
+            assert GameLogic.calculate_score([1, 2, 3])
+        assert str(err.value) == "Dice roll must be a tuple"
+
+    def test_gl_calculate_score_type_error_2(self):
+        """Test if the method raises TypeError when one of the tuple elements is not an integer"""
+        with pytest.raises(TypeError) as err:
+            assert GameLogic.calculate_score((1, 2, 'banana'))
+        assert str(err.value) == "Dice roll value must be integers"
+
+    def test_gl_roll_dice_type_error(self):
+        """Test if the method raises TypeError if not an integer passed in"""
+        with pytest.raises(TypeError) as err:
+            assert GameLogic.roll_dice(1.5)
+        assert str(err.value) == "The number of dice must be an integer"
+
+    def test_gl_roll_dice_value_error_1(self):
+        """Test if the method raises ValueError if the passed in integer is out of range"""
+        with pytest.raises(ValueError) as err:
+            assert GameLogic.roll_dice(0)
+        assert str(err.value) == "The number of dice must be between 1 and 6"
+
+
+class TestBanker:
+    """Tests for the Banker class and its methods
+    """
+    @pytest.fixture()
+    def banker(self):
+        return Banker()
+
+    def test_banker_instance(self):
+        """Check class instantiation"""
+        assert Banker()
