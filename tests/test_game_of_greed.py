c819e1e3ce2d2c2ae54dcdcc5c777eaf858cebbb
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-18 09:49:13 -0700

    minor refactor, comments added

--- tests/test_game_of_greed.py
+++ tests/test_game_of_greed.py
@@ -53,7 +53,6 @@ class TestGameLogic:
         assert len(GameLogic.roll_dice(6)) == 6
 
 
-
 class TestBanker:
     """Tests for the Banker class and its methods
     """
@@ -69,13 +68,15 @@ class TestBanker:
         with pytest.raises(TypeError) as err:
             assert banker.shelf('Points must be integer')
         assert str(err.value) == 'Points must be integer'
-    
+
     def test_banker_self_pass_1(self, banker):
+        """Test if the scores are added to the shelf"""
         banker.shelf(100)
         banker.shelf(50)
         assert banker.shelf_points == 150
 
     def test_banker_bank_pass_1(self, banker):
+        """Test if the scores are added to the bank"""
         assert banker.shelf_points == 0
         assert banker.bank_points == 0
         banker.shelf(150)
@@ -86,8 +87,9 @@ class TestBanker:
         assert banker.bank_points == 150
 
     def test_banker_clear_shelf_pass_1(self, banker):
-        assert banker.shelf_points == 0 
+        """Test if the scores are removed from the shelf"""
+        assert banker.shelf_points == 0
         banker.shelf(200)
-        assert banker.shelf_points == 200 
+        assert banker.shelf_points == 200
         banker.clear_shelf()
         assert banker.shelf_points == 0
